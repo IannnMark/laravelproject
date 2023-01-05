85792f1e664e0af934fe586f6f2ca6884f5f0875
ianmark8 <ianmarkmorga07@gmail.com>
2023-01-05 23:53:57 +0800

    commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,11 +8,11 @@
         <div class="collapse navbar-collapse" id="main-navigation"> 
         <ul class="navbar-nav">
 
-        <li class="nav-item"><a class="nav-link" href="{{route('customer.index')}}"><p><strong><i class="fa fa-user" aria-hidden="true"></i>Customer</strong></p></a> </li> 
+        <li class="nav-item"><a class="nav-link" href="{{route('customer.index')}}"><p><strong><i class="	fas fa-users" aria-hidden="true"></i>Customer</strong></p></a> </li> 
 
-        <li class="nav-item"> <a class="nav-link" href="{{route('employee.index')}}"><p><strong><i class="fa fa-user-md" aria-hidden="true"></i>Employee</strong></p></a> </li>  
+        <li class="nav-item"> <a class="nav-link" href="{{route('employee.index')}}"><p><strong><i class="fas fa-user-tie" aria-hidden="true"></i>Employee</strong></p></a> </li>  
 
-         <li class="nav-item"> <a class="nav-link" href="{{route('service.index')}}"><p><strong><i class="fa fa-phone" aria-hidden="true"></i>Services</strong></p></a> </li> 
+         <li class="nav-item"> <a class="nav-link" href="{{route('service.index')}}"><p><strong><i class="fa fa-wrench" aria-hidden="true"></i>Services</strong></p></a> </li> 
 
         <li class="nav-item"> <a class="nav-link" href="{{route('product.index')}}"><p><strong><i class="fa fa-product-hunt"  aria-hidden="true"></i>Products</strong></p></a></li> 
 
